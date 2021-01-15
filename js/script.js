de3a762c926759c94aab533e7193e6c782cc4711
sousajo <josesousa@rentalcars.com>
2021-01-15 00:21:26 +0000

    wip implement selected seats

--- js/script.js
+++ js/script.js
@@ -4,21 +4,62 @@ const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 
+poputaleUI();
+
 let ticketPrice = parseInt(movieSelect.value);
+
+// Save selected movie index and price
+
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("moviePrice", moviePrice);
+}
 
 // Update total seats and value
 function updateSelectCount() {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
   const selectedSeatsCount = selectedSeats.length;
 
+  // Copy selected seats into array
+  // Map through array
+  // return a new array indexes
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
 }
 
-// Movie select event
+// Get data from localstorage and populate UI
+function poputaleUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+  // console.log("selected seats = " + selectedSeats);
+  if (selectedSeats !== null && selectedSeats > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+  const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
 
+// Movie select event
 movieSelect.addEventListener("change", (e) => {
   ticketPrice = +e.target.value;
+
+  // console.log(
+  //   e.target.selectedIndex,
+  //   e.target.value,
+  //   e.target.options[e.target.selectedIndex].innerText
+  // );
+
+  setMovieData(e.target.selectedIndex, e.target.value);
+
   updateSelectCount();
 });
 
